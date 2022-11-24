dc72b909dab268a16ef58020ec3323af407a4db8
Colin Fraser <colin.fraser+schoolofcode@gmail.com>
2022-11-24 12:32:21 +0000

    Finalised createPost functionality using boolean conditions in useEffect

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,57 +8,62 @@ import logo from '../../sjardin-logo-green.svg'
 //import cors from 'cors';
 
 
+// when handleSubmit runs, set toggle to true 
+// use if condition in useEffect
+// when useEffect ends, set toggle to false
+
+
+
 function App() {
-  const [posts, setPosts] = useState(samplePosts); //empty this state when switching to live database 
+  //const [posts, setPosts] = useState(samplePosts); //empty this state when switching to live database 
+  const [posts, setPosts] = useState([]);
   const [buttonPopup, setButtonPopup] = useState(false);
+  const [newPost, setNewPost] = useState({});
+  const [toggle, setToggle] = useState(false);
   
   useEffect(() => {
     async function getAllposts() {
      let response = await fetch("http://localhost:3005/api/posts", {mode: "cors", method: "GET", headers: {"Content-Type": "application/json",}});
      let data = await response.json();
-     (console.log(data))
-     setPosts(data.payload);
+  
+     setPosts([...data.payload]);
+     (console.log("The data loaded: ", data))
+     console.log("The posts state: ", posts)
     }
     getAllposts();
   }, []);
 
   useEffect(() => {
 
-   
+  if (toggle){
+    async function createAPost(newPost) {
+      console.log("useEffect postObject from state: ", newPost)
+      const post = await fetch("http://localhost:3005/api/posts", 
+        {
+          method: "POST", 
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+          }, 
+          body: JSON.stringify(newPost)
+        }
+      );
 
-   async function createAPost(posts) {
-    // const tempBody = {
-    //   username: "te39248394",
-    //   projectgoal: "sdfdsfsdfThing",
-    //   projecttype: "Build",
-    //   projecttools: "SQL",
-    //   collaborators: 123
-    // };
-
-    // we need an alternative way of getting the created post
-    const postObject = posts[posts.length-1];
-
-
-
-    const post = await fetch("http://localhost:3005/api/posts", 
-    {
-      method: "POST", 
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-      }, 
-      body: JSON.stringify(postObject)
+      if (post.success) {
+        console.log("Post was success");    
+      }
+      setPosts([...posts, newPost])
+      //const data = await post.json();
     }
-    );
-    if (post.success) {
-      console.log("Post was success");    
-    }
-    //const data = await post.json();
-   }
-   createAPost(posts);
-   console.log('CreateAPost useEffect has worked')
-   // THIS DEPENDENCY DOES NOT WORK ON A DELETE REQUEST
-  }, [posts])
+
+    createAPost(newPost);
+    console.log('CreateAPost useEffect has worked')
+    setToggle(false);
+  } else {
+    console.log("Toggle is false! Hahah.");
+  }
+    // THIS DEPENDENCY DOES NOT WORK ON A DELETE REQUEST
+  }, [newPost])
 
   //JSON.stringify(posts[-1])
 
@@ -75,7 +80,10 @@ function App() {
   // on handleSubmit we wil have a function that spreads out the previous array of postObjects and append the new object
   function handleSubmit(postObject) {
     console.log('You clicked postObject');
-    setPosts([...posts, postObject])
+    setToggle(true);
+    setNewPost(postObject);
+
+    //setPosts([...posts, postObject])
     console.log("handleSubmit postObject: ", postObject)
   }
 
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -11,7 +11,7 @@ function SearchBar({posts}) {
     */
 
     return <div className="searchBar">
-        <label for="categories">I want to</label>
+        <label htmlFor="categories">I want to</label>
         <select name="categories" id="categories">
             <option value="build">build</option>
             <option value="study">study</option>
@@ -20,19 +20,19 @@ function SearchBar({posts}) {
 
         <label> using</label>
             <input type="checkbox" id="css" name="css" value="css"/>
-            <label for="css">CSS</label>
+            <label htmlFor="css">CSS</label>
             <input type="checkbox" id="express" name="express" value="express"/>
-            <label for="vehicle2">Express</label>
+            <label htmlFor="vehicle2">Express</label>
             <input type="checkbox" id="html" name="html" value="html"/>
-            <label for="vehicle3">HTML</label>
+            <label htmlFor="vehicle3">HTML</label>
             <input type="checkbox" id="js" name="js" value="js"/>
-            <label for="vehicle3">Javascript</label>
+            <label htmlFor="vehicle3">Javascript</label>
             <input type="checkbox" id="node" name="node" value="node"/>
-            <label for="vehicle3">Node</label>
+            <label htmlFor="vehicle3">Node</label>
             <input type="checkbox" id="sql" name="sql" value="sql"/>
-            <label for="vehicle3">SQL</label>
+            <label htmlFor="vehicle3">SQL</label>
             <input type="checkbox" id="react" name="react" value="react"/>
-            <label for="vehicle3">React</label>
+            <label htmlFor="vehicle3">React</label>
         <button onClick={handleClick}>Find me a seed</button>
     </div>
 }
