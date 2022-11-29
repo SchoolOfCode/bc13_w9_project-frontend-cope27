ab13fd1ebde6f767c2793788c216a24a51ac9669
Stephanie Adielé <stephanieadiele@stephanies-mbp.home>
2022-11-29 15:28:09 +0000

    started tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,98 +1,54 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import CreatePost from '../CreatePost/index'
-import ProjectBoard from '../ProjectBoard/index'
-import SearchBar from '../SearchBar/index'
-import userEvent from '@testing-library/user-event';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import userEvent from "@testing-library/user-event";
+import SearchBar from "../SearchBar";
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
-/* To test:
-img logo
-p tag text 
-button with text
-three components, CreatePost, ProjectBoard and SearchBar
-*/
-  
-
-it('find a button', () => {
-  const {getByText, container, debug} = render(<App />)
+test("to find create post button", () => {
+  const { getByText, container, debug } = render(<App />);
   const createPostButton = getByText("Create Post");
-
-  console.log(container.innerHTML);
 
   //const image = screen.getByRole('img');
   expect(createPostButton).toBeInTheDocument();
   //expect(image).toBeVisible();
-
 });
-
-test('that each element renders on the screen', () => {
-  render(<App />)
-
-  const image = screen.getByRole('img');
-  expect(image).toBeInTheDocument();
-  expect(image).toBeVisible();
-
-});
-
-test('the text in paragraph', () => {
-  render(<App />)
-
-  const paraElement = screen.getByText('collaboration', {exact: false});
-  expect(paraElement).toBeInTheDocument();
-  expect(paraElement).toBeVisible();
-
-});
-
-test('the text in paragraph again', () => {
-  render(<App />)
-
-  const paraElement = screen.getByText('collaboration', {exact: false});
-  expect(paraElement).toBeInTheDocument();
-  expect(paraElement).toBeVisible();
-
-});
-
-
-
-
-
-test('test that each button renders on the screen', () => {
-  render(<App />)
-
-  const button = screen.getByRole('button', {name:/Create Post/i});
+/** alternate way to test create post button render*/
+test("that each button renders on the screen", () => {
+  render(<App />);
+  const button = screen.getByRole("button", { name: /Create Post/i });
   expect(button).toBeInTheDocument();
   expect(button).toBeVisible();
-
 });
 
-test('that button click calls the createPost function', ()=> {
-  render(<App />)
+test("that the logo renders on screen", () => {
+  render(<App />);
+  const image = screen.getByRole("img");
+  expect(image).toBeInTheDocument();
+  expect(image).toBeVisible();
+});
 
-  const button = screen.getByRole('button', {name:/Create Post/i});
+test("that the slogan text appears on screen", () => {
+  render(<App />);
+  const paraElement = screen.getByText("collaboration", { exact: false });
+  expect(paraElement).toBeInTheDocument();
+  expect(paraElement).toBeVisible();
+});
+
+test("that button click calls the createPost function", () => {
+  render(<App />);
+  const button = screen.getByRole("button", { name: /Create Post/i });
   userEvent.click(button);
+});
 
-})
+test("that search bar is rendered", () => {
+  render(<App />);
+  const searchBar = screen.getByText("I want to");
+  expect(searchBar).toBeInTheDocument();
+  expect(searchBar).toBeVisible();
+});
 
-
-
-// test('that the paragraph of text renders and contains the right text', () => {
-//   render(<App />)
-
-//   let paragraphElement = screen.getByRole('paragraph',  {class:'slogan'});
-//   //expect(paragraphElement).toHaveClass("slogan");
-//   expect(paragraphElement).toBeVisible();
-
-// });
-
-// test('that it has a button', () => {
-//   render(<App/>);
-//   const button = screen.getByRole('button');
-
-//   expect(button).toBeVisible();
-// })
+test("that the project board is rendered", () => {
+  render(<App />);
+  const projectBoard = screen
+  expect(projectBoard).toBeInTheDocument();
+  expect(projectBoard).toBeVisible();
+});
