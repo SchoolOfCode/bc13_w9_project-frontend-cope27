36d49e4d0ae24654cc08cdb048489bcfcc9ed429
[Creg Grady] <112422397+CregCodes@users.noreply.github.com>
2022-11-30 13:45:02 +0000

    Completed tests for createpost & projectBoard

--- src/components/ProjectBoard/index.test.js
+++ src/components/ProjectBoard/index.test.js
@@ -1,18 +1,21 @@
 import { render, screen } from "@testing-library/react";
-import ProjectBoard from "./index.js";
+import ProjectBoard from "./index";
 import { it, test, expect, describe } from "@jest/globals";
 
 describe("ProjectBoard renders element for user", () => {
   it("ProjectBoard renders a ul element containing posts", () => {
-    const projectPost = {
-      username: "Steph",
-      projectgoal: "Build an API for helping people",
-      projecttype: "Build",
-      projecttools: "Express",
-      collaborators: "4",
-    };
-    render(<ProjectBoard posts={projectPost} />);
+    const posts = [
+      {
+        username: "Steph",
+        projectgoal: "Build an API for helping people",
+        projecttype: "Build",
+        projecttools: "Express",
+        collaborators: "4",
+      },
+    ];
+    render(<ProjectBoard posts={posts} />);
     const projectBoard = screen.getByRole("list");
-    expect(projectBoard).toBeInDocument();
+    console.log(projectBoard);
+    expect(projectBoard).toBeVisible();
   });
 });
